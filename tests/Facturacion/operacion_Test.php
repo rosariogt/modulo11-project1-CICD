abe65beabb1aa0789fdaf88d6ae39462d897c27d
Rosario <rosario7dev@gmail.com>
2024-06-25 22:49:49 -0400

    Proyecto de ci cd

---
+++ tests/Facturacion/operacion_Test.php
@@ -0,0 +1,17 @@
+<?php
+declare (strict_types=1);
+use PHPUnit\Framework\TestCase;
+class operacion_Test extends TestCase{
+    public function testFactorial():void{
+        
+        $this->assertEquals(50,50);
+    }
+    public function testuno():void
+    {
+        $this->assertTrue(true);
+    }
+    public function testdos():void{
+        $this->assertTrue(true);
+    }
+
+}
